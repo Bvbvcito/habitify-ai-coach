161f27b4b6b657435a640f39ae88f4386e672558
BladeRunner-Nexus <143112019+BladeRunner-Nexus@users.noreply.github.com>
2024-11-11 12:07:31 +0100

    First Commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,59 +1,32 @@
 "use client";
+import ModalWindow from "@/components/ModalWindow";
 import { useState, useEffect } from "react";
 
-export default function Home() {
-  // Define Reesponse Data Type
-  interface ResponseData {
-    response: {
-      name: string;
-      date: string;
-      // Add other properties as needed within the `data` object
-    };
-  }
-
-  const [responseData, setResponseData] = useState<ResponseData | null>(null);
-  const [error, setError] = useState<string | null>(null); // State for storing any errors
-
-  async function postData() {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ message: "This is a call from Next.js" }),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json(); // Convert response to JSON
-      setResponseData(data); // Set the response data to state
-    } catch (error) {
-      setError((error as Error).message); // Set the error message to state
-    }
-  }
-
-  useEffect(() => {
-    postData();
-  }, []);
+export default function HomePage() {
+  const [modal, setModal] = useState(false);
 
   return (
-    <div className="flex w-full h-screen items-center justify-center">
-      <div>
-        <h1>Hello Next Flask</h1>
-        {error ? (
-          <p className="text-red-500">Error: {error}</p>
-        ) : responseData ? (
-          <p>
-            Response from Flask: {responseData.response.name}, born{" "}
-            {responseData.response.date}
-          </p>
-        ) : (
-          <p>Loading...</p>
-        )}
+    <>
+      {
+        <ModalWindow
+          onClose={() => setModal(false)}
+          show={modal}
+          edit_method="Add"
+        />
+      }
+      <div className="container bg-gray-200 mx-auto h-full min-h-screen items-center justify-center flex">
+        <div>
+          <button
+            onClick={() => setModal(true)}
+            className="bg-orange-500 text-white rounded-full p-2"
+          >
+            Set Modal
+          </button>
+          <span className="text-gray-500">
+            {modal ? "La modal est ouverte" : "La modal est fermée"}
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
